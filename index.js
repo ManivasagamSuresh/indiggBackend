46f8e25c51f478ff67912d52302edf356cdd98e0
Manivasagam Suresh <manivasagam.suresh12@gmail.com>
2023-08-13 23:21:59 +0530

    url changed

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const user = require("./Routes/user")
 const tournament = require("./Routes/tournament")
 
 app.use(express.json());
-app.use(cors({origin:"http://localhost:3000"}));
+// app.use(cors({origin:"http://localhost:3000"}));
+app.use(cors({origin:"https://steady-shortbread-751776.netlify.app/"}));
 
 app.use("/api/user",user)
 app.use("/api/tournament",tournament)
